11092791943ba64522199eb14a68992463276489
mmongan <marty.mongan@gmail.com>
2026-03-22 08:31:58 -0500

    Move camera starting position to the center
    - Changed CAMERA_POSITION from (0, 0, -800) to (0, 0, 0)
    - Camera now starts at the center of the scene
    - Provides an immersive center view of the code dependency graph
    - Users can navigate from the center outward to explore connections

--- unitcirclevr/src/SceneConfig.ts
+++ unitcirclevr/src/SceneConfig.ts
@@ -36,7 +36,7 @@ export class SceneConfig {
   static readonly POINT_LIGHT_POSITION = new BABYLON.Vector3(5, 10, 5);
 
   // Camera
-  static readonly CAMERA_POSITION = new BABYLON.Vector3(0, 0, -800);  // Much further back to see full ±250 space
+  static readonly CAMERA_POSITION = new BABYLON.Vector3(0, 0, 0);  // Center of the scene
   static readonly CAMERA_INERTIA = 0.5;
   static readonly CAMERA_ANGULAR_SENSIBILITY = 1000;
 
